84ee3cb17f713d4666552ce036a6b9f646a90879
cyber space <37174373+cyber002@users.noreply.github.com>
2025-12-03 19:38:07 +0800

    Update request.js

--- search/scripts/request.js
+++ search/scripts/request.js
@@ -5,6 +5,7 @@ const services = [
 	{ name: 'Bilibili', icon: 'bilibili', url: 'https://www.bilibili.com/', action: 'select', requiresCodec: true },
 	{ name: 'Age动漫', icon: 'age', url: 'https://www.agedm.io/', action: 'select', requiresCodec: true },
 	{ name: 'Omofun动漫', icon: 'omofun', url: 'https://www.acgbibi.com/', action: 'select', requiresCodec: true },
+	{ name: 'cnflix影视', icon: 'cnflix', url: 'https://www.cnflix.tv/', action: 'select', requiresCodec: true },
 	{ name: 'YouTube', icon: 'youtube', url: 'https://youtube.com/', action: 'select', requiresCodec: false },
 	{ name: 'TikTok', icon: 'tiktok', url: 'https://tiktok.com/', action: 'select', requiresCodec: true },
 	{ name: 'SoundCloud', icon: 'soundcloud', url: 'https://soundcloud.com/discover', action: 'select', requiresCodec: false },
