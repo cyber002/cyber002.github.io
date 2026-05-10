0f566141fa25da93ec62afdb5d6220415534fee0
cyber space <37174373+cyber002@users.noreply.github.com>
2026-05-10 16:33:36 +0800

    Add files via upload

--- search/scripts/request.js
+++ search/scripts/request.js
@@ -2,10 +2,6 @@
 
 // Service definitions with codec requirements
 const services = [
-	{ name: 'Bilibili', icon: 'bilibili', url: 'https://www.bilibili.com/', action: 'select', requiresCodec: true },
-	{ name: 'Age动漫', icon: 'age', url: 'https://www.agedm.io/', action: 'select', requiresCodec: true },
-	{ name: 'Omofun动漫', icon: 'omofun', url: 'https://www.acgbibi.com/', action: 'select', requiresCodec: true },
-	{ name: 'cnflix影视', icon: 'cnflix', url: 'https://www.cnflix.tv/', action: 'select', requiresCodec: true },
 	{ name: 'YouTube', icon: 'youtube', url: 'https://youtube.com/', action: 'select', requiresCodec: false },
 	{ name: 'TikTok', icon: 'tiktok', url: 'https://tiktok.com/', action: 'select', requiresCodec: true },
 	{ name: 'SoundCloud', icon: 'soundcloud', url: 'https://soundcloud.com/discover', action: 'select', requiresCodec: false },
@@ -14,11 +10,10 @@ const services = [
 	{ name: 'Rumble', icon: 'rumble', url: 'https://rumble.com/', action: 'select', requiresCodec: true },
 	{ name: 'DLive', icon: 'dlive', url: 'https://dlive.tv/', action: 'select', requiresCodec: true },
 	{ name: 'Kick', icon: 'kick', url: 'https://kick.com/', action: 'select', requiresCodec: true },
+	{ name: 'Bilibili', icon: 'bilibili', url: 'https://www.bilibili.tv/', action: 'open', requiresCodec: true },
 	{ name: 'Archive', icon: 'archive', url: 'https://archive.org/details/movies', action: 'select', requiresCodec: true },
-	{ name: 'VKontakte', icon: 'vk', url: 'https://vk.com/video', action: 'select', requiresCodec: true },
-	{ name: 'Sibnet', icon: 'sibnet', url: 'https://video.sibnet.ru/rub/anime/', action: 'select', requiresCodec: true },
-	{ name: 'OK', icon: 'ok', url: 'https://ok.ru/video', action: 'select', requiresCodec: true },
-	{ name: 'Rutube', icon: 'rutube', url: 'https://rutube.ru/', action: 'open', requiresCodec: true }
+	{ name: 'VK Видео', icon: 'vk', url: 'https://vkvideo.ru/', action: 'select', requiresCodec: true },
+	{ name: 'Одноклассники', icon: 'ok', url: 'https://ok.ru/video', action: 'select', requiresCodec: true },
 ];
 
 // Codec support detection
@@ -59,9 +54,11 @@ async function initializeServices() {
 		}
 
 		card.innerHTML = `
-			<div class="service-icon logo-${service.icon}"></div>
-			<div class="service-name">${service.name}</div>
-			${isDisabled ? '<div class="disabled-overlay">Codec Required</div>' : ''}
+			<div class="service-card-inner">
+				<div class="service-icon logo-${service.icon}"></div>
+				<div class="service-name">${service.name}</div>
+				${isDisabled ? '<div class="disabled-overlay">Codec Required</div>' : ''}
+			</div>
 		`;
 
 		grid.appendChild(card);
